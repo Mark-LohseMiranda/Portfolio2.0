bd5fd402a3d0f6e5b1a63f54cfd48ae4c6e26569
mark.lohsemiranda <mark.lohsemiranda@gmail.com>
2023-05-12 14:03:11 -0700

    updated completed tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,106 +1,28 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import me from "./assets/apngb-animated.png";
-import sleep from "./assets/sleep.png";
-import ouch from "./assets/ouch.png";
-import Speech from "./Components/Speech/Speech";
 import Completed from "./Components/Projects/Completed";
-import "./App.css";
-
 import Navbar from "./Components/Navbar/Navbar";
 import Welcome from "./Components/Welcome/Welcome";
 import InProgress from "./Components/Projects/InProgress";
+import "./App.css";
 
 function App() {
-  const [message, setMessage] = useState("Hello");
-  const [mouseMoving, setMouseMoving] = useState(false);
 
-  // const mouseMove = () => {
-  //   setMouseMoving(true);
-  //   setTimeout(() => setMouseMoving(false), 10000);
-  // };
-
-  const getClickCoords = (event) => {
-    // from: https://stackoverflow.com/a/29296049/14198287
-    var e = event.target;
-    var dim = e.getBoundingClientRect();
-    var x = event.clientX - dim.left;
-    var y = event.clientY - dim.top;
-    // return [x, y];
-    // console.log(x, y);
-    if (x >= 23 && x <= 273 && y >= 97 && y <= 227) {
-      setMessage("Ouch!");
-      setTimeout(() => {
-        setMessage("Hello");
-      }, 1000);
-    } else if (x >= 61 && x <= 286 && y >= 324 && y <= 476) {
-      setMessage("That tickles!");
-      setTimeout(() => {
-        setMessage("Hello");
-      }, 1000);
-    }
-  };
-
-  // useEffect(() => {
-  //   const noMouseMove = () => {
-  //     setMouseMoving(false);
-  //   };
-  //   const handleMouseMove = () => {
-  //     setMouseMoving(true);
-  //     setTimeout(() => {
-  //       setMouseMoving(false);
-  //       // return;
-  //     }, 5000);
-  //   };
-  //   document.addEventListener("mousemove", handleMouseMove);
-    // window.addEventListener("mousedown", handleMouseMove);
-    // window.addEventListener("touchmove", handleMouseMove);
-    // window.addEventListener("scroll", handleMouseMove);
-    // return () => {
-    //   document.removeEventListener("mousemove", noMouseMove);
-    //   // window.removeEventListener("mousedown", noMouseMove);
-    //   // window.removeEventListener("touchmove", noMouseMove);
-    //   // window.removeEventListener("scroll", noMouseMove);
-    // };
-  // }, [setMouseMoving]);
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setMouseMoving(false);
-  //   }, 5000);
-  // }, [mouseMoving, setMouseMoving]);
+  const [navigation, setNavigation] = useState([
+    { name: "Welcome", href: "/", current: true },
+    { name: "In Progress Projects", href: "/inprogress", current: false },
+    { name: "Completed Projects", href: "/completed", current: false },
+  ])
 
   return (
-    // <
-    //   onTouchMove={() => setMouseMoving(true)}
-    //   onScroll={() => setMouseMoving(true)}
-    //   onMouseDown={() => setMouseMoving(true)}
-    //   onMouseMove={() => setMouseMoving(true)}
-    // >
     <div>
       <BrowserRouter>
-        <Navbar />
+        <Navbar navigation={navigation} setNavigation={setNavigation}/>
         <Routes>
           <Route path="/completed" element={<Completed />} />
           <Route path="/inprogress" element={<InProgress/>}/>
           <Route path="*" element={<Welcome/>}/>
         </Routes>
-
-        {/* {mouseMoving ? (
-          <div id="memoji">
-            {message === "Ouch!" ? (
-              <img onClick={getClickCoords} src={ouch} alt="sad memoji" />
-            ) : (
-              <img onClick={getClickCoords} src={me} alt="memoji" />
-            )}
-            <Speech message={message} />
-          </div>
-        ) : (
-          <div id="memoji">
-            <img onClick={getClickCoords} src={sleep} alt="sleeping memoji" />
-            
-          </div>
-        )} */}
       </BrowserRouter>
     </div>
   );
--- src/Components/Projects/Completed.jsx
+++ src/Components/Projects/Completed.jsx
@@ -5,21 +5,49 @@ import bulletjournalImg from "../../assets/bulletjournal.png";
 import gingerbread from "../../assets/gingerbread.jpg";
 
 export default function Completed() {
-
   return (
     <div className="container mx-auto">
       <div className="grid-cols-2 space-y-0 animate lg:grid lg:gap-3 lg:grid-rows-3">
-        <div>
+        <div className="">
           <Frame image={gingerbread} />
+        </div>
+        <div className="text-justify place-self-center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. In nulla
+          posuere sollicitudin aliquam ultrices. Leo vel orci porta non pulvinar
+          neque laoreet suspendisse interdum. Sed pulvinar proin gravida
+          hendrerit lectus. Interdum consectetur libero id faucibus nisl
+          tincidunt eget nullam. Imperdiet sed euismod nisi porta lorem mollis
+          aliquam ut. Pharetra sit amet aliquam id diam maecenas ultricies mi.
+          Risus quis varius quam quisque id diam vel quam.
         </div>
         <div>
           <Frame image={tabletopImg} />
         </div>
+        <div className="text-justify place-self-center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. In nulla
+          posuere sollicitudin aliquam ultrices. Leo vel orci porta non pulvinar
+          neque laoreet suspendisse interdum. Sed pulvinar proin gravida
+          hendrerit lectus. Interdum consectetur libero id faucibus nisl
+          tincidunt eget nullam. Imperdiet sed euismod nisi porta lorem mollis
+          aliquam ut. Pharetra sit amet aliquam id diam maecenas ultricies mi.
+          Risus quis varius quam quisque id diam vel quam.
+        </div>
         <div>
           <Frame image={bulletjournalImg} />
         </div>
+        <div className="text-justify place-self-center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. In nulla
+          posuere sollicitudin aliquam ultrices. Leo vel orci porta non pulvinar
+          neque laoreet suspendisse interdum. Sed pulvinar proin gravida
+          hendrerit lectus. Interdum consectetur libero id faucibus nisl
+          tincidunt eget nullam. Imperdiet sed euismod nisi porta lorem mollis
+          aliquam ut. Pharetra sit amet aliquam id diam maecenas ultricies mi.
+          Risus quis varius quam quisque id diam vel quam.
+        </div>
       </div>
-      
     </div>
   );
 }
